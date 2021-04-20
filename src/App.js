00c6f4a0ba7894ef93ea20dc5bf58608fa90c1f0
jawilkes232 <jordanwilkes2327@gmail.com>
2021-04-20 12:01:31 -0600

    maybe this react will work?

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import './App.css';
-import Amplify from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { AmplifySignOut } from '@aws-amplify/ui-react';
+
 Amplify.configure(awsconfig);
 
 function App() {
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		Hub.listen('auth', ({ payload: { event, data } }) => {
+			switch (event) {
+				case 'signIn':
+				case 'cognitoHostedUI':
+					getUser().then((userData) => setUser(userData));
+					break;
+				case 'signOut':
+					setUser(null);
+					break;
+				case 'signIn_failure':
+				case 'cognitoHostedUI_failure':
+					console.log('Sign in failure', data);
+					break;
+			}
+		});
+
+		getUser().then((userData) => setUser(userData));
+	}, []);
+
+	function getUser() {
+		return Auth.currentAuthenticatedUser()
+			.then((userData) => userData)
+			.catch(() => console.log('Not signed in'));
+	}
+
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<AmplifySignOut />
-				<h2>My App Content</h2>
-			</header>
+		<div>
+			<p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+			{user ? (
+				<button onClick={() => Auth.signOut()}>Sign Out</button>
+			) : (
+				<button onClick={() => Auth.federatedSignIn()}>
+					Federated Sign In
+				</button>
+			)}
 		</div>
 	);
 }
