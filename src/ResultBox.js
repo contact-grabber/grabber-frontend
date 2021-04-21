f03bd22fd934393fe77ae702da69bbb34c212065
jawilkes232 <jordanwilkes2327@gmail.com>
2021-04-21 15:30:59 -0600

    added search bar and basic components layed out

--- src/ResultBox.js
+++ src/ResultBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Results from './Results';
 
-const ResultBox = () => {
+const ResultBox = ({ lastSearch }) => {
 	return (
 		<div>
 			<h1>Results Box</h1>
+			<h3>Showing results for {lastSearch}</h3>
 			<Results />
 		</div>
 	);
