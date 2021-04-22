9219f45d08ca268b194426dc6469b1ffe8e73d7e
jawilkes232 <jordanwilkes2327@gmail.com>
2021-04-22 09:19:48 -0600

    added lambda function and updated APIGW

---
+++ amplify/backend/function/monsterfunction/amplify/backend/function/JobScraper/src/index.py
@@ -0,0 +1,43 @@
+import json
+from bs4 import BeautifulSoup
+from scraper_api import ScraperAPIClient
+import os
+
+
+def handler(event, context):
+
+    client = ScraperAPIClient(os.environ.get('API_KEY'))
+    URL = "https://www.indeed.com/jobs?q=Entry+Level+Software+Engineer&l=Remote"
+    page = client.get(url=URL)
+    print(page)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(soup)
+    results = soup.find(id='resultsCol')
+    print(results)
+    job_elems = results.find_all('div', class_='jobsearch-SerpJobCard')
+    titles = []
+    companies = []
+    summaries = []
+    for job_elem in job_elems:
+        title_elem = job_elem.find('h2', class_='title')
+        company_elem = job_elem.find('div', class_='sjcl')
+        summary_elem = job_elem.find('div', class_='summary')
+        titles.append(title_elem.text.strip())
+        companies.append(company_elem.text.strip())
+        summaries.append(summary_elem.text.strip())
+
+    response = {
+        "titles": titles,
+        "companies": companies,
+        "summaries": summaries
+    }
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(response)
+    }
---
+++ amplify/backend/function/monsterfunction/src/index.py
@@ -0,0 +1,32 @@
+import json
+from bs4 import BeautifulSoup
+from scraper_api import ScraperAPIClient
+import os
+
+def handler(event, context):
+    client = ScraperAPIClient('2873f1389a25c9d9d2fda418700fc835')
+    monster_url = "https://www.monster.com/jobs/search?q=Software+Engineer&where=Nashville%2C+TN&page=6"
+    page = client.get(url=monster_url, render=True)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_='results-page')
+    job_elems = results.find_all('div', class_='results-card')
+    titles = []
+    summaries = []
+    for job_elem in job_elems:
+        title_elem = job_elem.find('div', class_='title-company-location')
+        summary_elem = job_elem.find('div', class_='results-card-description')
+        titles.append(title_elem.text.strip())
+        summaries.append(summary_elem.text.strip())
+    response = {
+        "titles": titles,
+        "summaries": summaries
+    }
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(response)
+    }
