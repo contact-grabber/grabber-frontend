c904750327bb657883373e8f0b81efb9a6cbf767
jawilkes232 <jordanwilkes2327@gmail.com>
2021-04-23 16:01:17 -0600

    fixed scraper url

--- amplify/backend/function/monsterfunction/src/index.py
+++ amplify/backend/function/monsterfunction/src/index.py
@@ -30,8 +30,8 @@ def handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
         'body': result
