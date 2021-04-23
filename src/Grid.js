af992e45587ddfaa9b67678a2e8c4ed7c560dc15
jawilkes232 <jordanwilkes2327@gmail.com>
2021-04-23 08:00:53 -0600

    refactored lambda

---
+++ src/Grid.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Grid = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: stretch;
+	max-width: 100%;
+	flex-wrap: wrap;
+`;
+export default Grid;
--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import Grid from './Grid';
 
-const Results = () => {
+const Results = ({ state, setState, route }) => {
+	useEffect(() => {
+		axios
+			.get(
+				`https://gjgml13loc.execute-api.us-west-2.amazonaws.com/staging/${route}`
+			)
+			.then((res) => {
+				setState(res);
+			});
+	}, []);
+
 	return (
-		<div>
-			<p>I'm The Results!</p>
-		</div>
+		<Grid>
+			<div>
+				<p>I'm The Results!</p>
+				{state ? console.log(state) : null}
+				<p>{state ? state.data.titles[1] : 'Loading'}</p>
+			</div>
+		</Grid>
 	);
 };
 
